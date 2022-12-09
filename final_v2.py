00b7560242c99af03e4b8a2582ecc047a78ad656
rachelsondergeld <rsondergeld1@gmail.com>
2022-12-09 17:01:08 -0500

    Successfully created a table in the database that contains the geoname_id, the latitude and the longitude
    Successfully created a table in the database that contains the geoname_id, the latitude and the longitude

--- final_v2.py
+++ final_v2.py
@@ -14,7 +14,6 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-
 # get geo API data 
 def get_geoData():
     url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=geonames-all-cities-with-a-population-1000&q=&rows=200&sort=population&facet=feature_code&facet=cou_name_en&facet=timezone'
@@ -77,44 +76,44 @@ def add_region_info(data, cur, conn):
     conn.commit()
 
 #########################################
+
 #Creating a table with the lat/long data
 def create_lat_long_table(cur, conn):
     # specify primary key
-    cur.execute("CREATE TABLE IF NOT EXISTS coordinates (geoname_id INTEGER PRIMARY KEY, latitude TEXT, longitude TEXT)") 
+    cur.execute("CREATE TABLE IF NOT EXISTS AQI_AND_COORDINATES (geoname_id INTEGER PRIMARY KEY, latitude INTEGER, longitude INTEGER)") 
     conn.commit()
 
 #Adding Latitude and Longitude to a new table
 def add_lat_long_data(cur_row, data, cur, conn):
     # PUT THE DATA IN
     info = data
-    regions = []
     counter = 0
-
-
-    # make region list
-    for item in info['facet_groups'][2]['facets']:
-        region_name = item['name']
-        regions.append(region_name)
 
     # get 25 and add to table
     for item in info['records'][cur_row:]:
         if counter < 25:
             geoname_id = int(item['fields']['geoname_id'])
-            city_name = item['fields']['name']
-            country = item['fields']['cou_name_en']
-            population = int(item['fields']['population'])
-            region = (item['fields']['timezone']).split('/')[0]
-            region_id = int(regions.index(region))
-
+            latitude = item['fields']['coordinates'][0]
+            longitude = item['fields']['coordinates'][1]
             counter += 1
-            cur.execute("INSERT OR IGNORE INTO cities (geoname_id, city_name, country, population, region_id) VALUES (?,?,?,?,?)", (geoname_id, city_name, country, population, region_id))
+            cur.execute("INSERT OR IGNORE INTO AQI_AND_COORDINATES (geoname_id, latitude, longitude) VALUES (?,?,?)", (geoname_id, latitude, longitude))
         else:
             break
     conn.commit()
 
+#### Code below not copied, working with second API now
 
-def create_AQI_Table(cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS air_pollution_table (geoname_id INTEGER, Latitude INTEGER, Longitude INTEGER, AQI INTEGER)')
+#Getting AQI Data from API 2
+# def get_aqi_data():
+#     for row in [lat/long column in the database]:
+#         url = f"http://api.airvisual.com/v2/nearest_city?lat={lat}&lon={long}&key=5c55caf7-4f01-424c-a8ba-a5b474141637"
+#         payload={}
+#         headers = {}
+#         response = requests.request("GET", url, headers=headers, data=payload)
+#     print(response.text)
+
+# def add_AQI_data(cur, conn):
+#     cur.execute('INSERT OR IGNORE INTO AQI_AND_COORDINATES (geoname_id INTEGER, Latitude INTEGER, Longitude INTEGER, AQI INTEGER)')
 
 
 #########################################
@@ -140,6 +139,10 @@ def main():
     add_toppop_cities(rows, get_geoData(),cur, conn)
     add_region_info(get_geoData(), cur, conn)
 
+    #testing
+    #get_aqi_data()
+    create_lat_long_table(cur, conn)
+    add_lat_long_data(rows, get_geoData(),cur, conn)
 
 
 main()

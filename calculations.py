5d15e7a03976929709b66dccda38a0c45230d024
no <81531433+monoallana@users.noreply.github.com>
2022-12-11 16:14:07 -0500

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,19 @@
+# calculations + visualizations file > output visualizations and text file
+
+# creating a txt file to write to
+f = open('calculations.txt', 'w')
+f.close()
+
+# 1. counting how many most populated cities are in a specific region (cities table / region?) (Allana)
+f = open('test.txt', 'a')
+f.write('adding one.\n')
+f.write('\n')
+f.close()
+
+# 2. counting how many most populated cities are in specific AQI ranges (AQI info) (Rachel)
+
+
+# 3. average air pollutant concentration per by country (join) (Rachel)
+
+
+# 4. average current AQI/weather per region (join) (Allana)
